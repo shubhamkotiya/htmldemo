e06c52d6ac723f0c89f36b6625238a5de767956c
Ram Yadav <ramy51726@gmail.com>
2018-07-31 14:36:11 +0530

    initialize file

---
+++ fact.rb
@@ -0,0 +1,18 @@
+class Func
+  def ram(var1)
+    def fun(var)
+      if var==1 
+        return var
+      else
+        return (var*fun(var-1))
+      end
+    end
+    fun(var1)
+  end
+end
+c = Func.new
+c1 = Func.new
+var2=c.ram(5)
+puts (var2)
+
+var3 = c1.ram(8)
